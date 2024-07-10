d194d316bfae58a24b05001b5fd917180c56c66f
Uzair Ahmad <125276051+uzairahmad12389@users.noreply.github.com>
2024-07-10 10:25:17 +0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,18 @@
 import Alert from   './component/Alert';
 import Textform from './component/Textform';
+import About from './component/abou';
 import Navbar from './component/navbar';
-import Abou from'./component/abou';
+// import Abou from'./component/abou';
 import { useState} from 'react';
 import React from "react";
-import { BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
 function App() {
   const [mode, setMode] = useState("light");
-
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
@@ -29,25 +34,21 @@ function App() {
    
   return (
     <>
-    <Router>
+        <Router>
       <Navbar title="TextUtil" mode={mode} toggleMode={toggleMode} />
       {alertMessage && <Alert message={alertMessage} />}
-      <div className="container my-3">
       <Switch>
           <Route path="/about">
-            <Abou />
+            <About mode={mode} toggleMode={toggleMode} />
           </Route>
-          
+        
           <Route path="/">
           <Textform
           title="Enter your text" mode={mode} toggleMode={toggleMode}
-          showAlert={showAlert}
-        />
+          showAlert={showAlert}/>
           </Route>
         </Switch>
-       
-      </div>
-      </Router>
+        </Router>
       
     </>
   );
